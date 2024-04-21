4f710b209f5715e323dacd62dafb72769e76a806
Thushar K Nimbalkar <56183791+thusharkn@users.noreply.github.com>
2024-04-22 00:01:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ a = st.number_input("Enter first number", value=0)
 b = st.number_input("Enter second number", value=0)
 c = st.number_input("Enter third number", value=0)
 
-    if st.button("Find Largest"):
-        largest = max(a, b, c)
-        st.write(f"The largest number is: {largest}")
-
-if __name__ == "_main_":
-    main()
+if st.button("Find Largest"):
+    largest = max(a, b, c)
+    st.write(f"The largest number is: {largest}")
